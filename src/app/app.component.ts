a1960b4353c373cb0f80779cca3acf0e73f77eea
Dany Paredes <danywalls4@gmail.com>
2024-05-02 19:03:34 +0200

    feat: 🎸 initial setup
    create initial setup with service and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProductsService} from "./services/products.service";
+import {AsyncPipe, CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'kendo-store';
+  public title = 'testing-in-angular';
+  public products$ = inject(ProductsService).products$;
+
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+export type Product = {
+  id: string;
+  title: string;
+  image: string;
+  price: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+   private API = 'https://fakestoreapi.com/products'
+  private http = inject(HttpClient)
+  public products$ = this.http.get<Product[]>(this.API);
+}
